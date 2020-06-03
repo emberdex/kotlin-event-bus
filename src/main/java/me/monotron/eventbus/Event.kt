19984a924925356e48bb5fcee494ecfe7d7fd1dd
Toby Jones <github@monotron.me>
2020-06-03 19:10:45 +0100

    Make the clock value in events non-nullable

--- src/main/java/me/monotron/eventbus/Event.kt
+++ src/main/java/me/monotron/eventbus/Event.kt
@@ -3,7 +3,7 @@ package me.monotron.eventbus
 import java.time.Clock
 import java.time.LocalDateTime
 
-open class Event(var sender: Any? = null, val clock: Clock? = Clock.systemUTC()) {
+open class Event(var sender: Any? = null, val clock: Clock = Clock.systemUTC()) {
 
     var timeTriggered: LocalDateTime = LocalDateTime.now(clock)
 }
